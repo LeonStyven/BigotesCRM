41a9f64168749dd76308aca6ac4afd6b8bf8d722
Leon Styven <j.stca03@gmail.com>
2023-04-24 15:15:42 -0500

    Modal estilizado y funcional

--- js/main.js
+++ js/main.js
@@ -1,29 +1,15 @@
 const ModalStatus = false;
-
-document.getElementById("product-card").addEventListener('click', toggleModal);
 document.getElementById("close-modal-button").addEventListener('click', toggleModal);
+document.querySelectorAll(".product-card").forEach(element => {
+    element.addEventListener('click', e =>{
+        const id = e.target.getAttribute("id");
+        alert('Se ha clickeado el elemento: '+id);
+        toggleModal();
+        
+    });
+})
 
 
 function toggleModal(){
-    document.getElementById("modal-background").classList.toggle('modal-display')
-    if(ModalStatus){
-        disableScroll;
-    }else{
-        enableScroll;
-    }
-}
-
-function disableScroll() {
-    // Get the current page scroll position
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  
-        // if any scroll is attempted, set this to the previous value
-        window.onscroll = function() {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
-}
-  
-function enableScroll() {
-    window.onscroll = function() {};
+    document.getElementById("modal-background").classList.toggle('modal-display');
 }
