6a41aa9c6faa205b1315d994affd4bc6238b5943
Leon Styven <j.stca03@gmail.com>
2023-04-27 13:50:35 -0500

    Merge between branch santi and styven

---
+++ js/modalLogin.js
@@ -0,0 +1,23 @@
+let backBoxLogin = document.getElementById("back__box__login");
+let backBoxRegister = document.getElementById("back__box__register");
+let formLogin = document.getElementById("form__login");
+let formRegister = document.getElementById("form_register");
+let backBox = document.getElementById("back__box");
+document.getElementById("back-box-btn-login").addEventListener("click", changeLogin);
+document.getElementById("back-box-btn-register").addEventListener("click", changeRegister);
+
+function changeLogin(event){
+    backBoxLogin.classList.toggle("back-box-login-active");
+    backBoxRegister.classList.toggle("back-box-register-active");
+    formLogin.style.display = "flex";
+    formRegister.style.display = "none";
+    backBox.classList.toggle("back-box-reverse");   
+}
+
+function changeRegister(event){
+    backBoxLogin.classList.toggle("back-box-login-active");
+    backBoxRegister.classList.toggle("back-box-register-active");
+    formRegister.style.display = "flex";
+    formLogin.style.display = "none";
+    backBox.classList.toggle("back-box-reverse");  
+}
