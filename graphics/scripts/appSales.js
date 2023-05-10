cac6d88dd317fbbf2af53f08c3dcf0965ac0fc95
= <=>
2023-05-10 08:36:05 -0500

    Graphics With Data

---
+++ graphics/scripts/appSales.js
@@ -0,0 +1,82 @@
+const ctx1 = document.getElementById('myChart-1');
+const ctx2 = document.getElementById('myChart-2');
+const ctx3 = document.getElementById('myChart-3');
+const ctx4 = document.getElementById('myChart-4');
+const ctx5 = document.getElementById('myChart-5');
+const ctx6 = document.getElementById('myChart-6');
+
+new Chart(ctx1, {
+  type: 'bar',
+  data: {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
+    datasets: [{
+      label: 'Ventas Totales al Mes',
+      data: [12, 19, 20, 10, 25, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: ['2020', '2021', '2022', '2023'],
+    datasets: [{
+      label: 'Ventas Totales al Año',
+      data: [100, 160, 170, 50, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx3, {
+  type: 'bar',
+  data: {
+    labels: ['Atrapasueños Mediano', 'Llavero Mandala', 'Llavero Atrapasueños Verde'],
+    datasets: [{
+      label: 'Productos más Vendidos',
+      data: [80,71,70, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx4, {
+    type: 'bar',
+    data: {
+      labels: ['Llavero de Mascotras', 'Llavero Mandala', 'Atrapasueños Mediano', 'Llavero Rústico', 'Llavero Multicolor', 'Llavero Atrapasueños Verde', 'Llavero Atrapasueños Negro'],
+      datasets: [{
+        label: 'Ventas por Producto',
+        data: [40, 71, 80, 30, 50, 70, 10, 0],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
---
+++ graphics/scripts/appUsers.js
@@ -0,0 +1,120 @@
+const ctx1 = document.getElementById('myChart-1');
+const ctx2 = document.getElementById('myChart-2');
+const ctx3 = document.getElementById('myChart-3');
+const ctx4 = document.getElementById('myChart-4');
+const ctx5 = document.getElementById('myChart-5');
+const ctx6 = document.getElementById('myChart-6');
+
+new Chart(ctx1, {
+  type: 'bar',
+  data: {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
+    datasets: [{
+      label: 'Usuarios Registrados por Mes',
+      data: [12, 19, 20, 5, 25, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: ['2020', '2021', '2022', '2023'],
+    datasets: [{
+      label: 'Usuarios Registrados por Año',
+      data: [50, 60, 30, 81, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx3, {
+  type: 'bar',
+  data: {
+    labels: ['<18', '18-25', '26-30', '31<'],
+    datasets: [{
+      label: 'Numero Usuarios por Edad',
+      data: [40, 71, 80, 30, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx4, {
+  type: 'bar',
+  data: {
+    labels: ['Sí', 'No', 'Primer Pedido'],
+    datasets: [{
+      label: 'Porcentaje Usuarios que han Comprado',
+      data: [60,10,30, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx5, {
+  type: 'bar',
+  data: {
+    labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'],
+    datasets: [{
+      label: 'Numero de PQRSs por Mes',
+      data: [12, 19, 3, 5, 2, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+new Chart(ctx6, {
+  type: 'bar',
+  data: {
+    labels: ['2020', '2021', '2022', '2023'],
+    datasets: [{
+      label: 'Numero de PQRSs por Año',
+      data: [30, 18, 10, 41, 0],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
